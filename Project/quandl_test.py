acf5be66558a014518855f05359d0f3ef96cb3bf
DigheAmeya <digheameya.4@gmail.com>
2015-02-12 14:10:00 -0800

    test

--- Project/quandl_test.py
+++ Project/quandl_test.py
@@ -1,6 +1,10 @@
 import Quandl
 #mydata = Quandl.get("WIKI/AAPL")
-mydata = Quandl.get("FRED/GDP")
+#mydata = Quandl.get("FRED/GDP")
+#mydata = Quandl.get("OFDP/FUTURE_ESZ2014")
+#mydata = Quandl.get("OFDP/FUTURE_ADH1987")
+mydata = Quandl.get("YAHOO/ALMIL_PA")
+#mydata = Quandl.get("GOOG")
 print(mydata)
 mydata.describe
 
